27e25da4c22c9e6d7bba56a6546371cfaaf4d3bd
joseluishernandezclaros <joseluishc1966@gmail.com>
2023-04-06 16:15:06 -0500

    que bonito

---
+++ react-aprendizaje-cmd/src/Doble.js
@@ -0,0 +1,11 @@
+export function Doble() {
+  function add(n1, n2) {
+    return n1 + n2;
+  }
+
+  return (
+    <>
+      <h1>{add(10, 30)}</h1>
+    </>
+  );
+}
--- react-aprendizaje-cmd/src/index.js
+++ react-aprendizaje-cmd/src/index.js
@@ -1,5 +1,6 @@
 import React from "react"; //importar react
 import ReactDom from "react-dom"; //importar Dom
+import { Doble } from "./Doble"; //importar funcion doble que esta src
 
 const rootElement = document.getElementById("root"); //seleccionar root o raiz
 const root = ReactDom.createRoot(rootElement); //crear componente root
@@ -29,20 +30,57 @@ const root = ReactDom.createRoot(rootElement); //crear componente root
 //   </div>
 // ); //
 
-function JSX() {
-  // const name = 'fazt';
-  // const name1 = 30;
-  // return <h1>{name}<br></br>{name1+name1}</h1>
-  // const married = true;
-  // if(married){
-  //     return <h1>Estoy casado</h1>
-  // }else{
-  //     return <h1>No estoy casado</h1>
-  // }
+// function JSX() {
+// const name = 'fazt';
+// const name1 = 30;
+// return <h1>{name}<br></br>{name1+name1}</h1>
+// const married = true;
+// if(married){
+//     return <h1>Estoy casado</h1>
+// }else{
+//     return <h1>No estoy casado</h1>
+// }
 
-  const married = true;
-  // return married ? <h1>Estoy casado</h1> : <h1>No estoy casado</h1>
-  return <h1>{married ? "Estoy casado 😁" : "No estoy casado 🙃"} </h1>;
-}
+//   const married = true;
+//   // return married ? <h1>Estoy casado</h1> : <h1>No estoy casado</h1>
+//   return <h1>{married ? "Estoy casado 😁" : "No estoy casado 🙃"} </h1>;
+// }
 
-root.render(<JSX/>);
+// root.render(<JSX />);
+
+// function Objetos() {
+//   const user = {
+//     firstName: "ryan",
+//     lastName: "ray",
+//   };
+//   // return <h1>{JSON.stringify(user)}</h1>;
+//   // JSON.stringify() convertir objeto de JS en su version String
+
+//   return (
+//     <>
+//       <h1>{user.firstName}</h1>
+//       <h3>{user.lastName}</h3>
+//     </>
+//   );
+// }
+
+// root.render(
+//   <>
+//     <Objetos />
+//     <Objetos />
+//     <Objetos />
+//     <Objetos />
+//   </>
+// );
+
+
+
+
+root.render(
+  <>
+    <Doble/>
+    <Doble/>
+    <Doble/>
+    <Doble/>
+  </>
+);
